bc3e38c2aaebe0e24ae311d75f5ce62018132292
april <nkzhangzhang@gmail.com>
2020-11-15 16:30:04 -0800

    added functionalities to course-navigator

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseNavigatorComponent implements OnInit {
 
-  constructor() { }
+  courses = [];
+  modules = [];
+  lessons = [];
+  topics = [];
+  selectedCourse = {
+    title: ''
+  };
+
+  createCourse = () =>
+    this.courseService.createCourse()
+      .then(course => this.courses.push(course))
+
+  deleteCourse = (course) =>
+    this.courseService.deleteCourse(course._id)
+      .then(status => this.courses = this.courses.filter(c => c._id !== course._id))
+
+  editCourse = (course) =>
+    course.editing = true
+
+  saveCourse = (course) => {
+    course.editing = false;
+    this.courseService.updateCourse(course);
+  }
+
+  selectCourse = (course) => {
+    this.selectedCourse = course;
+    this.moduleService.findModulesForCourse(course._id)
+      .then(modules => this.modules = modules);
+  }
+
+  createModuleForCourse = (selectedCourse) =>
+    this.moduleService.createModuleForCourse(selectedCourse._id)
+      .then(module => this.modules.push(module))
+
+  deleteModule = (module) =>
+    this.moduleService.deleteModules(module._id)
+      .then(status => this.modules = this.modules.filter(m => m._id !== module._id))
+
+  saveModule = (module) =>
+    this.moduleService.updateModule(module)
+      .then(status => this.modules = this.modules.map(m => m._id === module._id ? module : m))
+
+  constructor(private courseService: CourseService,
+              private moduleService: ModuleService) { }
 
   ngOnInit(): void {
+    this.courseService.findAllCourses()
+      .then(courses => this.courses = courses);
   }
 
 }
