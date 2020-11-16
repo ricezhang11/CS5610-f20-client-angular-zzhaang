2261eaebf7ee724d176f84f1ea1644413dae8bc3
april <nkzhangzhang@gmail.com>
2020-11-15 16:09:31 -0800

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import { TopicPillsComponent } from './topic-pills/topic-pills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseNavigatorComponent,
+    CourseListComponent,
+    LessonTabsComponent,
+    ModuleListComponent,
+    TopicPillsComponent
   ],
   imports: [
     BrowserModule,
