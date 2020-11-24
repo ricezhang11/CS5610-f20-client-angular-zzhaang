bfa4e45b14ff6016f25ed7faf7e68abfa621644d
april <nkzhangzhang@gmail.com>
2020-11-23 16:43:23 -0800

    changed app name

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/cs5610-client-angular-azhang'));
+app.use(express.static(__dirname + '/dist/cs5610-f20-client-angular-aprilzhang'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+ '/dist/cs5610-client-angular-azhang/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/cs5610-f20-client-angular-aprilzhang/index.html'));
 });
 app.listen(process.env.PORT || 8080);
