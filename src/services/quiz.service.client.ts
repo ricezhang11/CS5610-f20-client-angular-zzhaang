269a605c26718f5060fced9c55c02f54e0f09623
april <nkzhangzhang@gmail.com>
2020-11-23 20:24:11 -0800

    change url to remote server on heroku

--- src/services/quiz.service.client.ts
+++ src/services/quiz.service.client.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class QuizzesServiceClient {
-  findAllQuizzes = () => fetch('http://localhost:3000/api/quizzes')
+  findAllQuizzes = () => fetch('https://cs5610-server-node-azhang.herokuapp.com/api/quizzes')
     .then(response => response.json())
-  findQuizById = (qid: any) => fetch(`http://localhost:3000/api/quizzes/${qid}`)
+  findQuizById = (qid: any) => fetch(`https://cs5610-server-node-azhang.herokuapp.com/api/quizzes/${qid}`)
     .then(response => response.json())
 }
