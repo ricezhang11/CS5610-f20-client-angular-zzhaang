473e4dcdcdf1173309d6858dd3eed691163f8470
april <nkzhangzhang@gmail.com>
2020-12-03 11:36:32 -0800

    configure for heroku deployment

--- src/services/attempt.service.client.ts
+++ src/services/attempt.service.client.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class AttemptServiceClient {
-  findAttemptById = (aid: any) => fetch(`http://localhost:3000/api/attempts/${aid}`)
+  findAttemptById = (aid: any) => fetch(`https://cs5610-server-node-azhang.herokuapp.com/api/attempts/${aid}`)
     .then(response => response.json())
 }
--- src/services/quiz.service.client.ts
+++ src/services/quiz.service.client.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class QuizzesServiceClient {
-  findAllQuizzes = () => fetch('http://localhost:3000/api/quizzes')
+  findAllQuizzes = () => fetch('https://cs5610-server-node-azhang.herokuapp.com/api/quizzes')
     .then(response => response.json())
-  findQuizById = (qid: any) => fetch(`http://localhost:3000/api/quizzes/${qid}`)
+  findQuizById = (qid: any) => fetch(`https://cs5610-server-node-azhang.herokuapp.com/api/quizzes/${qid}`)
     .then(response => response.json())
-  findAttemptsForQuiz = (qzid: any) => fetch(`http://localhost:3000/api/quizzes/${qzid}/attempts`)
+  findAttemptsForQuiz = (qzid: any) => fetch(`https://cs5610-server-node-azhang.herokuapp.com/api/quizzes/${qzid}/attempts`)
     .then(response => response.json())
-  createAttemptForQuiz = (qzid: any, questions: any) => fetch(`http://localhost:3000/api/quizzes/${qzid}/attempts`,
+  createAttemptForQuiz = (qzid: any, questions: any) =>
+    fetch(`https://cs5610-server-node-azhang.herokuapp.com/api/quizzes/${qzid}/attempts`,
     {
       method: 'POST',
       body: JSON.stringify(questions),
